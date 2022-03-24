2a3698afc98b9fd5bc850f748321d0f1b7f19fed
rick <rickbthomason@yahoo.com>
2022-03-24 19:02:32 -0400

    change file name to main.py

---
+++ Day 1/main.py
@@ -0,0 +1,12 @@
+# how to print to the console using print function
+print('hello world')
+
+# strings using newline
+print('hello world\nhello world')
+
+# concactenating strings
+print('hello' + 'zoe')
+print('hello' + ' ' + 'zoe')
+
+# using the input function
+len(input('What is your name? '))
